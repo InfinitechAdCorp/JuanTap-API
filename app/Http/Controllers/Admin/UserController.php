f36900c6d454f5ec0bec03135c2c74dfd1baf9df
Infinitech-MosesAlcantara <infinitech.mosesalcantara@gmail.com>
2025-02-19 10:39:23 +0800

    update

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -63,31 +63,24 @@ class UserController extends Controller
             'password' => 'required|min:8',
         ]);
 
-        $code = 200;
+        $record = Model::where('email', $validated['email'])->first();
+        $isValid = Hash::check($validated['password'], $record->password);
 
-        $response = [
-            'email' => $validated['email'],
-            'password' => $validated['password'],
-        ];
-
-        // $record = Model::where('email', $validated['email'])->first();
-        // $isValid = Hash::check($validated['password'], $record->password);
-
-        // if ($record && $isValid) {
-        //     $record->tokens()->delete();
-        //     $token = $record->createToken("$record->email-AuthToken")->plainTextToken;
-        //     $code = 200;
-        //     $response = [
-        //         'message' => 'Logged In Successfully',
-        //         'token' => $token,
-        //         'record' => $record,
-        //     ];
-        // } else {
-        //     $code = 401;
-        //     $response = [
-        //         'message' => 'Invalid Credentials',
-        //     ];
-        // }
+        if ($record && $isValid) {
+            $record->tokens()->delete();
+            $token = $record->createToken("$record->email-AuthToken")->plainTextToken;
+            $code = 200;
+            $response = [
+                'message' => 'Logged In Successfully',
+                'token' => $token,
+                'record' => $record,
+            ];
+        } else {
+            $code = 401;
+            $response = [
+                'message' => 'Invalid Credentials',
+            ];
+        }
         return response()->json($response, $code);
     }
 
