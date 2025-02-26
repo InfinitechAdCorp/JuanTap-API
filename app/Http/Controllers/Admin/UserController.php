4c72161b0499dd0394a81216be8f69dec45fad39
Infinitech-MosesAlcantara <infinitech.mosesalcantara@gmail.com>
2025-02-26 15:39:10 +0800

    update password

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -205,23 +205,15 @@ class UserController extends Controller
     {
         $user_id = $request->header('user-id');
         $validated = $request->validate([
-            'old' => 'nullable|min:8|max:255',
             'new' => 'nullable|min:8|max:255',
         ]);
 
         $record = Model::find($user_id);
-        $isValid = Hash::check($validated['old'], $record->password);
-        if ($isValid) {
-            $record->update([
-                'password' => Hash::make($validated['new']),
-            ]);
-            $code = 200;
-            $response = ['message' => "Updated Password", 'record' => $record];
-        } else {
-            $code = 422;
-            $response = ['message' => "Invalid Password"];
-        }
-
+        $record->update([
+            'password' => Hash::make($validated['new']),
+        ]);
+        $code = 200;
+        $response = ['message' => "Updated Password", 'record' => $record];
         return response()->json($response, $code);
     }
 }
