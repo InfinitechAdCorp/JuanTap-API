fea0c3c8b64589cc8dfdf45efd419ff381ba9839
Infinitech-MosesAlcantara <infinitech.mosesalcantara@gmail.com>
2025-03-06 09:16:18 +0800

    upd

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -17,7 +17,7 @@ class DashboardController extends Controller
         $counts['templates'] = Template::get()->count();
         $counts['tickets'] = Ticket::get()->count();
 
-        $templates = Template::with('user')->get();
+        $templates = Template::with('users')->get();
 
         $response = ['message' => "Fetched Data", 'counts' => $counts, 'templates' => $templates];
         $code = 200;
