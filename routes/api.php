80d2fbe77ec78fcf50b54ee9f64427cf622ea976
Infinitech-MosesAlcantara <infinitech.mosesalcantara@gmail.com>
2025-03-07 10:08:41 +0800

    upd

--- routes/api.php
+++ routes/api.php
@@ -10,17 +10,6 @@ use App\Http\Controllers\API\SubscriptionController;
 use App\Http\Controllers\API\TicketController;
 use App\Http\Controllers\API\PaymentController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::prefix('auth')->group(function () {
     Route::post('by-email', [AuthController::class, 'getByEmail']);
     Route::post('link-oauth', [AuthController::class, 'linkOAuth']);
