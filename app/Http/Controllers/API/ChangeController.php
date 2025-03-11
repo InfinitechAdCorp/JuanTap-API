60fe57aa77cdcf69e48605637261160e7c337d8e
Infinitech-MosesAlcantara <infinitech.mosesalcantara@gmail.com>
2025-03-11 14:09:21 +0800

    changes

---
+++ app/Http/Controllers/API/ChangeController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Change as Model;
+
+class ChangeController extends Controller
+{
+    public $model = "Social";
+
+    public $rules = [
+        'name' => 'required|max:255',
+        'type' => 'required|max:255',
+        'description' => 'required',
+        'date' => 'required|date',
+    ];
+
+    public function getAll()
+    {
+        $records = Model::get();
+        $response = ['message' => "Fetched $this->model" . "s", 'records' => $records];
+        $code = 200;
+        return response()->json($response, $code);
+    }
+
+    public function get($id)
+    {
+        $record = Model::where('id', $id)->first();
+        if ($record) {
+            $response = ['message' => "Fetched $this->model", 'record' => $record];
+            $code = 200;
+        } else {
+            $response = ['message' => "$this->model Not Found"];
+            $code = 404;
+        }
+        return response()->json($response, $code);
+    }
+
+    public function create(Request $request)
+    {
+        $validated = $request->validate($this->rules);
+
+        $record = Model::create($validated);
+
+        $response = [
+            'message' => "Created $this->model",
+            'record' => $record,
+        ];
+        $code = 201;
+        return response()->json($response, $code);
+    }
+
+    public function update(Request $request)
+    {
+        $this->rules['id'] = 'required|exists:changes,id';
+        $validated = $request->validate($this->rules);
+
+        $record = Model::find($validated['id']);
+        $record->update($validated);
+
+        $response = ['message' => "Updated $this->model", 'record' => $record];
+        $code = 200;
+        return response()->json($response, $code);
+    }
+
+    public function delete($id)
+    {
+        $record = Model::find($id);
+        if ($record) {
+            $record->delete();
+            $response = ['message' => "Deleted $this->model"];
+            $code = 200;
+        } else {
+            $response = ['message' => "$this->model Not Found"];
+            $code = 404;
+        }
+        return response()->json($response, $code);
+    }
+}
